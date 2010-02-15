61ff4c829605e61ae7a8672c29dc8b5cc392a05e
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2010-02-16 00:10:25 +0100

    Remove Polish letter

--- external-fake/src/main/java/com/google/groups/warszawajug/money/CustomerDao.java
+++ external-fake/src/main/java/com/google/groups/warszawajug/money/CustomerDao.java
@@ -24,7 +24,7 @@ public class CustomerDao implements CustomerDaoLocal {
 		accountPrefixToOwner.put("2", new AccountOwner("Hans Moore", Locale.GERMANY.getDisplayCountry(Locale.US), "Dortmund", "Wilhelmstrasse 76", "08-984"));
 		accountPrefixToOwner.put("3", new AccountOwner("Maria Vincenza", Locale.ITALIAN.getDisplayCountry(Locale.US), "Palermo", "Via Vittorio Emanuele", "65-335"));
 		accountPrefixToOwner.put("4", new AccountOwner("Francois Lappe", Locale.FRANCE.getDisplayCountry(Locale.US), "Rennes", "Rue Gambetta 87", "86-991"));
-		accountPrefixToOwner.put("5", new AccountOwner("Jan Kowalski", new Locale("pl", "PL").getDisplayCountry(Locale.US), "Wrocław", "Kazimierza Wielkiego 34", "50-077"));
+		accountPrefixToOwner.put("5", new AccountOwner("Jan Kowalski", new Locale("pl", "PL").getDisplayCountry(Locale.US), "Wroclaw", "Kazimierza Wielkiego 34", "50-077"));
 		accountPrefixToOwner.put("6", new AccountOwner("GreatSoft Inc.", Locale.US.getDisplayCountry(Locale.US), "San Francisco", "Golden Gate Ave 5", "99-865"));
 		accountPrefixToOwner.put("7", new AccountOwner("Charity org.", Locale.US.getDisplayCountry(Locale.US), "Washington D.C.", "NW 54th St. 95", "11-294"));
 		accountPrefixToOwner.put("8", new AccountOwner("Anna Nowak", new Locale("pl", "PL").getDisplayCountry(Locale.US), "Szczecin", "Plac Grunwaldzki 7", "70-001"));
